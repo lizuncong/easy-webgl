8d1fbe8ebd972abf129da6e071705f02a95fc573
lizuncong <2900170896@qq.com>
2023-02-28 23:23:06 +0800

    feat: shader传值

--- src/shader传值/index.js
+++ src/shader传值/index.js
@@ -5,39 +5,38 @@ const gl = canvas.getContext("webgl");
 
 let vertexSource = `
     attribute vec2 a_position;
-    attribute float a_size;
+    uniform float u_size;
+    varying vec2 v_position;
     void main(){
+        v_position = a_position;
         gl_Position = vec4(a_position, 0.0, 1.0);
-        gl_PointSize = a_size;
+        gl_PointSize = u_size;
     }
 `;
 
 let fragmentSource = `
+    precision mediump float;
+    uniform vec3 u_color;
+    varying vec2 v_position;
     void main(){
-        gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);
+        gl_FragColor = vec4(u_color, 1.0);
     }
 `;
 
 const program = initShaders(gl, vertexSource, fragmentSource);
 
-console.log("program...", program);
 // 清空canvas画布
 gl.clearColor(0.5, 0.5, 0.5, 1.0); // rgba()
 gl.clear(gl.COLOR_BUFFER_BIT);
 
-let x = 0;
-let y = 0;
-let n = 1000;
-for (let i = 0; i < n; i++) {
-  const r = 0.5;
-  x = Math.sin(i) * r;
-  y = Math.cos(i) * r;
-  const a_position = gl.getAttribLocation(program, "a_position");
-  gl.vertexAttrib2f(a_position, x, y);
+const a_position = gl.getAttribLocation(program, 'a_position')
+gl.vertexAttrib2f(a_position, -0.6, 0.0)
 
-  const a_size = gl.getAttribLocation(program, "a_size");
-  gl.vertexAttrib1f(a_size, 10.0);
+const u_color = gl.getUniformLocation(program, 'u_color')
+gl.uniform3f(u_color, 1.0, 1.0, 0.0)
 
-  // 画一个点
-  gl.drawArrays(gl.POINTS, 0, 1);
-}
+const u_size = gl.getUniformLocation(program, 'u_size')
+gl.uniform1f(u_size, 40.0)
+
+// 画一个点
+gl.drawArrays(gl.POINTS, 0, 1);
