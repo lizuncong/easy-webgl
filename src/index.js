f00662e4d790abf5557bcac98133cab9c96fd643
lizuncong <2900170896@qq.com>
2023-04-02 23:19:40 +0800

    feat: stash

--- src/index.js
+++ src/index.js
@@ -1,8 +1,41 @@
-const canvas = document.getElementById('webgl')
+const canvas = document.getElementById("webgl");
 
-const gl = canvas.getContext('webgl')
+const gl = canvas.getContext("webgl");
 
+const VSHADER_SOURCE = `
+    void main() {
+        gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
+        gl_PointSize = 10.0;
+    }
+`;
 
-gl.clearColor(0,0,0,1)
-console.log('COLOR_BUFFER_BIT..', gl.COLOR_BUFFER_BIT)
-gl.clear(gl.COLOR_BUFFER_BIT)
+const FSHADER_SOURCE = `
+    void main(){
+        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    }
+`;
+
+const initShaders = (gl, vsource, fsource) => {
+  const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+  gl.shaderSource(vertexShader, vsource);
+  gl.compileShader(vertexShader);
+
+  const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER)
+  gl.shaderSource(fragmentShader, fsource)
+  gl.compileShader(fragmentShader)
+
+  const program = gl.createProgram();
+  gl.attachShader(program, vertexShader)
+  gl.attachShader(program,fragmentShader)
+
+  gl.linkProgram(program)
+  gl.useProgram(program)
+  gl.program = program
+};
+
+initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE);
+
+gl.clearColor(0, 0, 0, 1);
+gl.clear(gl.COLOR_BUFFER_BIT);
+
+gl.drawArrays(gl.POINTS, 0, 1);
