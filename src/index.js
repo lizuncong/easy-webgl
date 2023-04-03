9d13f95ec541746af0bce0d905b8573388142e68
lizuncong <2900170896@qq.com>
2023-04-03 23:24:06 +0800

    feat: stash

--- src/index.js
+++ src/index.js
@@ -4,9 +4,19 @@ const gl = canvas.getContext("webgl");
 
 const VSHADER_SOURCE = `
     attribute vec4 a_Position;
+    attribute float a_Size;
+    attribute float a_Size1;
+    attribute float a_Size2;
+    attribute float a_Size3;
+    attribute float a_Size4;
+    attribute float a_Size5;
+    attribute float a_Size6;
+    attribute float a_Size7;
+    attribute float a_Size8;
+    attribute float a_Size9;
     void main() {
         gl_Position = a_Position;
-        gl_PointSize = 10.0;
+        gl_PointSize = a_Size + a_Size1 + a_Size2 + a_Size3 + a_Size4 + a_Size5 + a_Size6 + a_Size7 + a_Size8 + a_Size9;
     }
 `;
 
@@ -36,7 +46,30 @@ const initShaders = (gl, vsource, fsource) => {
 
 initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE);
 const a_Position = gl.getAttribLocation(gl.program, 'a_Position')
+const a_Size = gl.getAttribLocation(gl.program, 'a_Size')
+const a_Size1 = gl.getAttribLocation(gl.program, 'a_Size1')
+const a_Size2 = gl.getAttribLocation(gl.program, 'a_Size2')
+const a_Size3 = gl.getAttribLocation(gl.program, 'a_Size3')
+const a_Size4 = gl.getAttribLocation(gl.program, 'a_Size4')
+const a_Size5 = gl.getAttribLocation(gl.program, 'a_Size5')
+const a_Size6 = gl.getAttribLocation(gl.program, 'a_Size6')
+const a_Size7 = gl.getAttribLocation(gl.program, 'a_Size7')
+const a_Size8 = gl.getAttribLocation(gl.program, 'a_Size8')
+const a_Size9 = gl.getAttribLocation(gl.program, 'a_Size9')
+
+console.log('a_Position....', a_Position, a_Size9)
 gl.vertexAttrib3f(a_Position, 0.5, 0.0, 0.0)
+gl.vertexAttrib1f(a_Size, 10.0)
+gl.vertexAttrib1f(a_Size1, 1.0)
+gl.vertexAttrib1f(a_Size2, 1.0)
+gl.vertexAttrib1f(a_Size3, 1.0)
+gl.vertexAttrib1f(a_Size4, 1.0)
+gl.vertexAttrib1f(a_Size5, 1.0)
+gl.vertexAttrib1f(a_Size6, 1.0)
+gl.vertexAttrib1f(a_Size7, 1.0)
+gl.vertexAttrib1f(a_Size8, 1.0)
+gl.vertexAttrib1f(a_Size9, 1.0)
+
 gl.clearColor(0, 0, 0, 1);
 gl.clear(gl.COLOR_BUFFER_BIT);
 
