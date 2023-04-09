68d4a0b58b394af17b9c0f824e9ba12a2dd89fe7
lizuncong <2900170896@qq.com>
2023-04-09 22:35:35 +0800

    feat: stash

--- src/index.js
+++ src/index.js
@@ -4,17 +4,22 @@ const gl = canvas.getContext("webgl");
 
 const VSHADER_SOURCE = `
     attribute vec4 a_Position;
-    attribute float a_PointSize;
+    attribute vec4 a_Color;
+    varying vec4 v_Color;
     void main() {
         gl_Position = a_Position;
-        gl_PointSize = a_PointSize;
+        gl_PointSize = 10.0;
+        v_Color = a_Color;
     }
 `;
 
 const FSHADER_SOURCE = `
     precision mediump float;
+    varying vec4 v_Color;
+    uniform float u_Width;
+    uniform float u_Height;
     void main(){
-        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+        gl_FragColor = vec4(gl_FragCoord.x / u_Width, 0.0, gl_FragCoord.y/u_Height, 1.0);
     }
 `;
 
@@ -41,32 +46,36 @@ initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE);
 
 const initVertexBuffers = (gl) => {
   const vertices = new Float32Array([
-    0.0, 0.5, -0.5, -0.5, 0.5, -0.5
+    0.0, 0.5, 1.0, 0.0, 0.0,
+    -0.5, -0.5, 0.0, 1.0, 0.0,
+    0.5, -0.5, 0.0, 0.0, 1.0
   ])
-  const sizes = new Float32Array([
-    10.0, 30.0, 60.0
-  ])
+  
 
   const n = 3;
 
   const vertexBuffer = gl.createBuffer();
-  const sizeBuffer = gl.createBuffer();
+
+  const FSIZE = vertices.BYTES_PER_ELEMENT;
 
   gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer)
   gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW)
 
   const a_Position = gl.getAttribLocation(gl.program, "a_Position");
 
-  gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0)
+  gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, FSIZE * 5, 0)
   gl.enableVertexAttribArray(a_Position)
 
-  gl.bindBuffer(gl.ARRAY_BUFFER, sizeBuffer)
-  gl.bufferData(gl.ARRAY_BUFFER, sizes, gl.STATIC_DRAW)
-  const a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize')
+  const a_Color = gl.getAttribLocation(gl.program, 'a_Color')
+  gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, FSIZE * 5, FSIZE * 2)
+  gl.enableVertexAttribArray(a_Color)
 
-  gl.vertexAttribPointer(a_PointSize, 1, gl.FLOAT, false, 0, 0)
-  gl.enableVertexAttribArray(a_PointSize)
+  const u_Width = gl.getUniformLocation(gl.program, 'u_Width')
+  gl.uniform1f(u_Width, gl.drawingBufferWidth)
 
+  const u_Height = gl.getUniformLocation(gl.program, 'u_Height')
+  gl.uniform1f(u_Height, gl.drawingBufferHeight)
+  console.log('gl..', gl.drawingBufferWidth, gl.drawingBufferHeight)
   return n
 }
 
@@ -76,5 +85,5 @@ const n = initVertexBuffers(gl)
 
 gl.clearColor(0.0, 0.0, 0.0, 1.0)
 gl.clear(gl.COLOR_BUFFER_BIT)
-gl.drawArrays(gl.POINTS, 0, n)
+gl.drawArrays(gl.TRIANGLES, 0, n)
 
