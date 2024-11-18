362bb18fa04b5d81b2df206941c4da58a60149fb
lizuncong <2900170896@qq.com>
2024-11-19 00:10:05 +0800

    feat: stash

--- docs/WebGLFundamentals/exercise/index.js
+++ docs/WebGLFundamentals/exercise/index.js
@@ -2,32 +2,38 @@ import initShaders from "./initShaders.js";
 
 
 const main = () => {
-    const canvas = document.getElementById('webgl')
-    const gl = canvas.getContext('webgl')
-    if (!gl) {
-        return
-    }
-    console.log('gl...', gl)
-    const vertexShaderSource = document.getElementById('vertex-shader-2d').text
-    const fragmentShaderSource = document.getElementById('fragment-shader-2d').text
+  const canvas = document.getElementById('webgl')
+  const gl = canvas.getContext('webgl')
+  if (!gl) {
+    return
+  }
+  console.log('gl...', gl)
+  const vertexShaderSource = document.getElementById('vertex-shader-2d').text
+  const fragmentShaderSource = document.getElementById('fragment-shader-2d').text
 
-    const program = initShaders(gl, vertexShaderSource, fragmentShaderSource)
+  const program = initShaders(gl, vertexShaderSource, fragmentShaderSource)
 
-    const positionLocation = gl.getAttribLocation(program, 'a_position')
+  const positionLocation = gl.getAttribLocation(program, 'a_position')
 
-    const positionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
-    const positions = [
-      0.0, 0.5,
-      0,0, 0.0,
-      0.5, 0.0  
-    ]
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW)
+  const positionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
+  const positions = [
+    0.5, 0.5,
+    0.0, 0.0,
+    0.5, 0.0
+  ]
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW)
+  gl.clearColor(0, 0, 0, 0)
+  gl.clear(gl.COLOR_BUFFER_BIT);
 
-    gl.useProgram(program)
+  gl.useProgram(program)
+  gl.vertexAttrib2f(positionLocation, 0.0, 0.5)
 
-    // gl.enable
-    console.log('location...', positionLocation)
+  gl.enableVertexAttribArray(positionLocation)
+  gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0)
+  console.log('location...', positionLocation)
+  // gl.disableVertexAttribArray(positionLocation)
+  gl.drawArrays(gl.POINTS, 0, 1)
 }
 
 main();
