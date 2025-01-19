02fd8131fcd41e787193043cf6e7ac493f74a5ae
lizuncong <2900170896@qq.com>
2025-01-20 00:01:03 +0800

    feat: 首页readme

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import shape from '../../../README.md'
+import MarkDown from "@/components/markdown";
 
 function Index() {
   return <div className="home">
     <div>首页</div>
+    <MarkDown src={shape}/>
   </div>;
 }
 
