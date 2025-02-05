b0abf6302a44432c2d1cd1d949718d15d0b8d259
lizuncong <2900170896@qq.com>
2025-02-06 01:44:23 +0800

    feat: 纹理尺寸限制:

--- demo/index.js
+++ demo/index.js
@@ -46,15 +46,18 @@ const main = (image) => {
     var texture = gl.createTexture();
     gl.bindTexture(gl.TEXTURE_2D, texture);
   
-    // // 设置参数，让我们可以绘制任何尺寸的图像
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
   
     // 将图像上传到纹理
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
-  
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    // gl.generateMipmap(gl.TEXTURE_2D);
+
+    // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
+    // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST_MIPMAP_NEAREST);
+
     const rectX = 0.5, rectY =  0.5;
     let verticesInfo = [
       0.0, rectY,
@@ -97,6 +100,7 @@ const main = (image) => {
   
   const image = new Image();
   image.src = "./cat.jpeg";  // 必须在同一域名下
+  // image.src = "./f.png";  // 必须在同一域名下
 
   image.onload = function () {
     main(image);
