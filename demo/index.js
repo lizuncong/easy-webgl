0f73d907d2a7ea8fff7c0f7abe88cffe5e91aec2
lizuncong <2900170896@qq.com>
2025-02-09 23:51:30 +0800

    feat: 纹理

--- demo/index.js
+++ demo/index.js
@@ -49,7 +49,7 @@ const main = (image) => {
   
     // 将图像上传到纹理
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
     // gl.generateMipmap(gl.TEXTURE_2D);
